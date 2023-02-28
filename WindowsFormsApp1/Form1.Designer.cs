b1fe8ab8182b3fa2e9ac6e33764e33d41ef21517
disskretnost <disskretnost@mail.ru>
2023-02-28 23:27:50 +0300

    первая форма с багами

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -31,14 +31,15 @@ namespace WindowsFormsApp1
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.LoginField = new System.Windows.Forms.TextBox();
+            this.PasswordField = new System.Windows.Forms.TextBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.registerlabel = new System.Windows.Forms.Label();
             this.resetLabel = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -48,37 +49,48 @@ namespace WindowsFormsApp1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bell MT", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(200, 154);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(164, 183);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(171, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Авторизация";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // LoginField
             // 
-            this.textBox1.Location = new System.Drawing.Point(166, 199);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(240, 49);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.LoginField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.LoginField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LoginField.Font = new System.Drawing.Font("Bell MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginField.ForeColor = System.Drawing.Color.White;
+            this.LoginField.Location = new System.Drawing.Point(137, 234);
+            this.LoginField.Multiline = true;
+            this.LoginField.Name = "LoginField";
+            this.LoginField.Size = new System.Drawing.Size(240, 45);
+            this.LoginField.TabIndex = 2;
+            this.LoginField.TextChanged += new System.EventHandler(this.LoginField_TextChanged);
             // 
-            // textBox3
+            // PasswordField
             // 
-            this.textBox3.Location = new System.Drawing.Point(166, 273);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(240, 51);
-            this.textBox3.TabIndex = 4;
+            this.PasswordField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.PasswordField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PasswordField.Font = new System.Drawing.Font("Bell MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordField.ForeColor = System.Drawing.Color.White;
+            this.PasswordField.Location = new System.Drawing.Point(137, 298);
+            this.PasswordField.MinimumSize = new System.Drawing.Size(240, 45);
+            this.PasswordField.Multiline = true;
+            this.PasswordField.Name = "PasswordField";
+            this.PasswordField.Size = new System.Drawing.Size(240, 45);
+            this.PasswordField.TabIndex = 4;
+            this.PasswordField.UseSystemPasswordChar = true;
+            this.PasswordField.TextChanged += new System.EventHandler(this.PasswordField_TextChanged);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(100, 199);
+            this.pictureBox2.Location = new System.Drawing.Point(87, 234);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(51, 49);
+            this.pictureBox2.Size = new System.Drawing.Size(44, 45);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
@@ -86,9 +98,9 @@ namespace WindowsFormsApp1
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(100, 273);
+            this.pictureBox1.Location = new System.Drawing.Point(85, 298);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(51, 51);
+            this.pictureBox1.Size = new System.Drawing.Size(46, 45);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -96,28 +108,36 @@ namespace WindowsFormsApp1
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(227, 25);
+            this.pictureBox3.Location = new System.Drawing.Point(194, 55);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(108, 109);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(331, 364);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(147)))), ((int)(((byte)(147)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(170, 382);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 44);
+            this.button1.Size = new System.Drawing.Size(154, 46);
             this.button1.TabIndex = 8;
             this.button1.Text = "Войти";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // registerlabel
             // 
             this.registerlabel.AutoSize = true;
             this.registerlabel.Font = new System.Drawing.Font("Bell MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.registerlabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.registerlabel.Location = new System.Drawing.Point(80, 480);
+            this.registerlabel.ForeColor = System.Drawing.Color.White;
+            this.registerlabel.Location = new System.Drawing.Point(90, 470);
             this.registerlabel.Name = "registerlabel";
             this.registerlabel.Size = new System.Drawing.Size(128, 27);
             this.registerlabel.TabIndex = 9;
@@ -128,32 +148,49 @@ namespace WindowsFormsApp1
             // 
             this.resetLabel.AutoSize = true;
             this.resetLabel.Font = new System.Drawing.Font("Bell MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resetLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.resetLabel.Location = new System.Drawing.Point(291, 480);
+            this.resetLabel.ForeColor = System.Drawing.Color.White;
+            this.resetLabel.Location = new System.Drawing.Point(265, 470);
             this.resetLabel.Name = "resetLabel";
             this.resetLabel.Size = new System.Drawing.Size(160, 27);
             this.resetLabel.TabIndex = 10;
             this.resetLabel.Text = "Забыли пароль?";
+            // 
+            // Close
+            // 
+            this.Close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Close.Font = new System.Drawing.Font("Bell MT", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Close.ForeColor = System.Drawing.Color.White;
+            this.Close.Location = new System.Drawing.Point(432, 9);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(28, 32);
+            this.Close.TabIndex = 11;
+            this.Close.Text = "X";
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            this.Close.MouseLeave += new System.EventHandler(this.Close_MouseLeave);
+            this.Close.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Close_MouseMove);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(526, 535);
+            this.ClientSize = new System.Drawing.Size(472, 544);
+            this.Controls.Add(this.Close);
             this.Controls.Add(this.resetLabel);
             this.Controls.Add(this.registerlabel);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PasswordField);
+            this.Controls.Add(this.LoginField);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -165,14 +202,15 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox LoginField;
+        private System.Windows.Forms.TextBox PasswordField;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label registerlabel;
         private System.Windows.Forms.Label resetLabel;
+        private System.Windows.Forms.Label Close;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,13 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        Point coordinate;
         public Form1()
         {
             InitializeComponent();
+           // this.PasswordField.AutoSize = false;
+            //this.PasswordField.Size = new Size(this.PasswordField.Size.Width, 40);
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,19 +36,64 @@ namespace WindowsFormsApp1
 
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void registerlabel_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close(); // закрываем приложение
+            Close.ForeColor = Color.Green;
+        }
+
+        private void LoginField_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void PasswordField_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close_MouseLeave(object sender, EventArgs e)
+        {
+            Close.ForeColor = Color.White;
+        }
+
+        private void Close_MouseMove(object sender, MouseEventArgs e)
+        {
+            Close.ForeColor = Color.FromArgb(1,147,147);
+        }
+        
+        //Передвижение первой формы//
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - coordinate.X; // передвижение по осям
+                this.Top += e.Y - coordinate.Y;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            coordinate = new Point(e.X, e. Y); // устанавливаем изначальное место
+        }
+        ///////////////////////////////////////////////////////
     }
 }
